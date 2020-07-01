1a799894107d3c5f8906ba09e34f987eba8a1c2f
John Smith <app.mobile.368@gmail.com>
2020-07-01 11:09:32 -0700

    Initial Commit

---
+++ GestureRecognizerPart2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestureRecognizerPart2
+//
+//  Created by NextUser on 7/1/20.
+//  Copyright © 2020 Swift&Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
